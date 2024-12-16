170830e8c3e2c902c55c695d7beb1df7dcfc4c2d
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-12-16 15:02:42 +0200

    Add files via upload

---
+++ session5.dart
@@ -0,0 +1,63 @@
+//import 'dart:math';
+
+/*int calculateXY(int x, int y) {
+  int c = x + y;
+  return c;
+}*/
+/*void x() {
+  for (int i = 1; i <= 10; i++) {
+    print("mohamed");
+  }
+}*/
+/*void calculatexyz(int x, int y, int z) {
+  int c = x + y + z;
+  print("The sum of x, y, and z is: $c ");
+}*/
+// parameter and return type
+/*int add(int a, int b) {
+  var total;
+  total = a + b;
+  return total;
+}
+
+// parameter and no return type
+void mul(int a, int b) {
+  var total;
+  total = a * b;
+  print("Multiplication is : $total");
+}
+
+// no parameter and return type
+String greet() {
+  String greet = "Welcome";
+  return greet;
+}
+
+// no parameter and no return type
+void greetings() {
+  print("Hello World!!!");
+}*/
+//void name() => print("my name");
+void main() {
+  /*int a = calculateXY(50, 10);
+  print("The sum of x and y is: $a ");*/
+  //x();
+  //calculatexyz(10, 20, 30);
+  /*var total = add(2, 3);
+  print("Total sum: $total");
+  mul(2, 3);
+  var greeting = greet();
+  print("Greeting: $greeting");
+  greetings();*/
+  //name();
+  /*Random random = new Random();
+  int x = random.nextInt(10);
+  print("the numbers generated from the random between 0 and 9 : $x ");*/
+  /*Random random = new Random();
+  bool y = random.nextBool();
+  print(y);*/
+  /*bool y = Random().nextBool();
+  print(y);*/
+  /*double z = Random().nextDouble();
+  print("The random double number between 0.0 and 1.0: $z ");*/
+}
